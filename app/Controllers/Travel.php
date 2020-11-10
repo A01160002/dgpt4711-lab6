3263f2cd20d4d4c768e5d65265c33c0025e2a14a
33950 <33950@DESKTOP-CM1MCPP>
2020-11-10 11:47:56 +0800

    change the baseURL in App.php and add template folder with two file bottom and top. Then add code in Travel.php to display data by view.

--- app/Controllers/Travel.php
+++ app/Controllers/Travel.php
@@ -11,10 +11,38 @@ namespace App\Controllers;
          
          //get a template parser
          $parser = \Config\Services::parser();
-         //tell it about the substitions
-         return $parser->setData(['records' => $records])
-                 // and have it render the template with those
-                 ->render('placeslist');
+         //tell it about the substitions    
+         
+         $table = new \CodeIgniter\View\Table();
+
+         $headings = $places->fields;
+         $displayHeadings = array_slice($headings, 1, 2);
+         $table->setHeading(array_map('ucfirst', $displayHeadings));
+         
+         foreach ($records as $record) {
+             $nameLink = anchor("travel/showme/$record->id",$record->name);
+             $table->addRow($nameLink,$record->description);
+         }
+         
+         $template = [
+             'table_open' => '<table cellpadding="10px">',
+             'cell_start' => '<td style="border: 5px solid #FF0000;">',
+             'row_alt_start' => '<tr style="background-color:#7E3D76">',
+             ];
+         $table->setTemplate($template);
+         
+         $fields = [
+             'title' => 'Travel Destinations',
+             'heading' => 'Travel Destinations',
+             'footer' => 'Liu Chang'
+             ];
+          return $parser->setData($fields)
+                         ->render('templates\top') .
+                  $table->generate() .
+                  $parser->setData($fields)
+                         ->render('templates\bottom');
+
+         return $table->generate(); 
      }
      
      public function showme($id)
@@ -27,8 +55,37 @@ namespace App\Controllers;
          // get a template parser     
          $parser = \Config\Services::parser();     
          // tell it about the substitions     
-         return $parser->setData($record)         
-                    // and have it render the template with those         
-                    ->render('oneplace');
+         
+         $table = new \CodeIgniter\View\Table();
+
+         $headings = $places->fields;
+//         $table->setHeading(array_map('ucfirst', $displayHeadings));
+         
+        $table->addRow($headings[0],$record['id']);
+        $table->addRow($headings[1],$record['name']);
+        $table->addRow($headings[2],$record['description']);
+        $table->addRow($headings[3],$record['link']);
+        $table->addRow($headings[4],"<img src=\"/image/".$record['image']."\"/>");
+        
+      
+         $template = [
+             'table_open' => '<table cellpadding="10px">',
+             'cell_start' => '<td style="border: 5px solid #FF0000;">',
+             'row_alt_start' => '<tr style="background-color:#7E3D76">',
+             ];
+         $table->setTemplate($template);
+         
+         $fields = [
+             'title' => 'Travel Destinations',
+             'heading' => 'Travel Destinations',
+             'footer' => 'Liu Chang'
+             ];
+          return $parser->setData($fields)
+                         ->render('templates\top') .
+                  $table->generate() .
+                  $parser->setData($fields)
+                         ->render('templates\bottom');
+
+         return $table->generate(); 
      }
  }
